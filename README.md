cc47b7cba66829298a532248f025f78d1096bcd1
kvii <13721952202@163.com>
2024-01-10 15:47:36 +0800

    feat: add reproduce document; add logging middleware for server.

--- README.md
+++ README.md
@@ -11,8 +11,31 @@
 改动部分：
 * [proto 文件](./api/helloworld/v1/greeter.proto)
 * [service 逻辑](./internal/service/myservice.go)
+* [client 测试代码](./internal/data/client_test.go)
 
-复现方法：
+客户端有 bug，复现方法：
+
+1. `kratos run`
+2. `go test -run TestHttpClient ./internal/data` （或者用 IDE 打开 client 测试代码手动点击测试）。
+3. 观察控制台输出。
+
+本来应该能正常请求的，结果 404：
+
+```sh
+--- FAIL: TestHttpClient (0.00s)
+    client_test.go:27: error: code = 404 reason =  message =  metadata = map[] cause = proto: syntax error (line 1:1): unexpected token 404
+FAIL
+FAIL    github.com/kvii/issue3141/internal/data 2.620s
+FAIL
+```
+
+看服务端控制台日志。实际上请求的 url 是 "/my-api/" 而非 "/my-api/id"，所以 404 了。
+
+```sh
+INFO ts=2024-01-10T15:41:11+08:00 caller=server/http.go:38 service.id=192.168.1.66 service.name= service.version= trace.id= span.id= module=http msg=request method=POST url=/my-api/
+```
+
+服务端是正常的，复现方法：
 
 1. `kratos run`
 2. `curl http://localhost:8000/my-api/1 -X POST -H 'Content-Type: application/json' -d '{"msg":[{}]}'`
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,13 +1,14 @@
 package server
 
 import (
-	v1 "github.com/kvii/issue3141/api/helloworld/v1"
-	"github.com/kvii/issue3141/internal/conf"
-	"github.com/kvii/issue3141/internal/service"
+	nt "net/http"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	v1 "github.com/kvii/issue3141/api/helloworld/v1"
+	"github.com/kvii/issue3141/internal/conf"
+	"github.com/kvii/issue3141/internal/service"
 )
 
 // NewHTTPServer new an HTTP server.
@@ -28,5 +29,14 @@ func NewHTTPServer(c *conf.Server, greeter *service.MyServiceService, logger log
 	}
 	srv := http.NewServer(opts...)
 	v1.RegisterMyServiceHTTPServer(srv, greeter)
+
+	// Add middleware for logging all the http request, include 404 routes.
+	h := srv.Handler
+	helper := log.NewHelper(log.With(logger, "module", "http"))
+	srv.Handler = nt.HandlerFunc(func(w nt.ResponseWriter, r *nt.Request) {
+		helper.Infow("msg", "request", "method", r.Method, "url", r.URL.String()) // no status code
+		h.ServeHTTP(w, r)
+	})
+
 	return srv
 }
